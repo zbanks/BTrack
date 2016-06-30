fd80fdb069c77fac902c468a4bc6f687be8c6b22
Zach Banks <zjbanks@gmail.com>
2016-06-29 22:49:49 -0400

    Add include guards to common.h

--- src/common.h
+++ src/common.h
@@ -1,3 +1,6 @@
+#ifndef __BTRACK_COMMON_H
+#define __BTRACK_COMMON_H
+
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -14,3 +17,5 @@ typedef float btrack_chunk_t;
 #define BTRACK_STRINGIFY(x) BTRACK_STRINGIFY2(x)
 #define BTRACK_STRINGIFY2(x) #x
 #define BTRACK_ASSERT(x) if(!(x)){fprintf(stderr,"Error, assertion failed: " __FILE__ " line %d: " BTRACK_STRINGIFY(x) "\n", __LINE__); exit(EXIT_FAILURE);}
+
+#endif
